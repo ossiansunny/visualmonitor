b34d3b672c660cdfa3dd1573ac6a5ba10c78deb5
ossiansunny <ossiansunny@gmail.com>
2025-04-30 13:57:04 +0900

    2025/4/30 phplinux replace all

--- phplinux/ubin/mrtgrun.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-htdocs=$1
-${htdocs}/ubin/logwriter.sh 'mrtgrun.sh' 'start mrtgrun.sh' $htdocs
-env LANG=C /usr/bin/mrtg ${htdocs}/mrtg/newmrtg.cfg >/dev/null 2>&1
-${htdocs}/ubin/logwriter.sh 'mrtgrun.sh' 'end mrtgrun.sh' $htdocs
---
+++ phplinux/vmsetup/init-varread.php
@@ -0,0 +1,38 @@
+<?php
+$path_vmsetup=__DIR__;
+$path_kanshiphpini=$path_vmsetup."/kanshiphp.ini";
+///
+function pathget($path){
+  global $path_kanshiphpini;
+  $rtnarr=array();
+  foreach($path as $arg){
+    $fp = fopen($path_kanshiphpini,"r");
+    while ($line = fgets($fp)) {
+      if(! (substr($line,0,2)=="//" or empty(trim($line)))){
+        $item=explode("=",$line);
+        $key=trim($item[0]);
+        $value=trim($item[1]);
+        $value=str_replace('"','',$value);
+        if($arg==$key){
+          array_push($rtnarr,$value);
+          break;
+        }
+      }
+    }
+    fclose($fp);
+  }  
+  return $rtnarr;
+}
+
+/*
+/// 配列にキー文字列を指定、その順にパスが配列で返る
+/// 取得の判定は、配列の要素数で行い、通常は要求した要素数でチェックする
+$patharr=array("vpath_mrtghome","vpath_weblog","vpath_kanshiphp");
+$pathfile=pathget($patharr);
+if(count($pathfile)==3){
+  var_dump($pathfile);
+}else{
+  var_dump($pathfile);
+}
+*/
+?>
